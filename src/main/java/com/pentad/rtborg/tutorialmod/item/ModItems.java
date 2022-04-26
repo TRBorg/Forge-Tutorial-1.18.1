4d04ea7511889048d3db2d07111e8b560523c7f8
tracy <tracy@pentad.com>
2022-04-26 16:42:58 -0700

    Weapon Effects and custom Armor

--- src/main/java/com/pentad/rtborg/tutorialmod/item/ModItems.java
+++ src/main/java/com/pentad/rtborg/tutorialmod/item/ModItems.java
@@ -2,11 +2,20 @@ package com.pentad.rtborg.tutorialmod.item;
 
 import com.pentad.rtborg.tutorialmod.TutorialMod;
 import com.pentad.rtborg.tutorialmod.item.custom.DowsingRodItem;
+import com.pentad.rtborg.tutorialmod.item.custom.LevitationSwordItem;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.*;
+import net.minecraft.world.level.Level;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+import java.util.function.Supplier;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
@@ -25,7 +34,7 @@ public class ModItems {
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB).food(ModFoods.CUCUMBER)));
 
     public static final RegistryObject<Item> CITRINE_SWORD = ITEMS.register("citrine_sword",
-            () -> new SwordItem(ModTiers.CITRINE, 2, 3f, new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
+            () -> new LevitationSwordItem(ModTiers.CITRINE, 2, 3f, new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
 
     public static final RegistryObject<Item> CITRINE_PICKAXE = ITEMS.register("citrine_pickaxe",
             () -> new PickaxeItem(ModTiers.CITRINE, 1, 1f, new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
@@ -37,7 +46,25 @@ public class ModItems {
             () -> new AxeItem(ModTiers.CITRINE, 4, 0, new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
 
     public static final RegistryObject<Item> CITRINE_HOE = ITEMS.register("citrine_hoe",
-            () -> new HoeItem(ModTiers.CITRINE, 0, 0f, new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
+            () -> new HoeItem(ModTiers.CITRINE, 0, 0f,
+                    new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)) {
+                @Override
+                public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+                    pTooltipComponents.add(new TranslatableComponent("tooltip.tutorialmod.item.citrine_hoe"));
+                }
+            });
+
+    public static final RegistryObject<Item> CITRINE_HELMET = ITEMS.register("citrine_helmet",
+            () -> new ArmorItem(ModArmorMaterials.CITRINE, EquipmentSlot.HEAD, new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
+
+    public static final RegistryObject<Item> CITRINE_CHESTPLATE = ITEMS.register("citrine_chestplate",
+            () -> new ArmorItem(ModArmorMaterials.CITRINE, EquipmentSlot.CHEST, new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
+
+    public static final RegistryObject<Item> CITRINE_LEGGINGS = ITEMS.register("citrine_leggings",
+            () -> new ArmorItem(ModArmorMaterials.CITRINE, EquipmentSlot.LEGS, new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
+
+    public static final RegistryObject<Item> CITRINE_BOOTS = ITEMS.register("citrine_boots",
+            () -> new ArmorItem(ModArmorMaterials.CITRINE, EquipmentSlot.FEET, new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
